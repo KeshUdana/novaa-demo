89307f3e6812dfd12808a0a6b8ad262b01c468c7
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-05-25 22:53:21 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex w-screen flex-col flex-wrap items-center justify-center text-balance mt-8">
       <h1>This is the Home</h1>
     </main>
   );
